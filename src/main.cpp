d79823d3bebbb329df8462182fca1e2609318dcc
Frederik Tobner <masterhorst90@web.de>
2023-10-26 23:41:47 +0200

    showing project version

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,19 @@
 
 #include "../libs/core/src/SDL_Instance.hpp"
 
+#include "nutra-config.hpp"
+
+#include <format>
+#include <iostream>
+
+
 int main(int argc, char * argv[]) {
+    std::cout << std::format("{} {}.{}.{}", PROJECT_NAME, PROJECT_VERSION_MAJOR, PROJECT_VERSION_MINOR,
+                             PROJECT_VERSION_PATCH)
+              << std::endl;
     try {
         auto sdl  = std::make_shared<Nutra::Core::SDL_Instance>();
-        auto game = std::make_unique<Nutra::Game::Application>(sdl, "Game");
+        auto game = std::make_unique<Nutra::Game::Application>(sdl, PROJECT_NAME);
         game->run();
     } catch (std::exception & e) {
         std::cerr << e.what() << std::endl;
