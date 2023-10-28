199cd9445e2d86ab0980267128b9a52e8af61ed4
Julian Otto <julxott@outlook.de>
2023-10-28 16:20:33 +0200

    feat: Add spdlog

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,20 @@
 
 #include "nutra_config.hpp"
 
+#include "spdlog/spdlog.h"
+
 #include <format>
 #include <iostream>
 
 auto main(int argc, char * argv[]) -> int {
-    std::cout << std::format("{} {}.{}.{}\n", PROJECT_NAME, PROJECT_VERSION_MAJOR, PROJECT_VERSION_MINOR,
+    spdlog::info("Starting {} version {}.{}.{}", PROJECT_NAME, PROJECT_VERSION_MAJOR, PROJECT_VERSION_MINOR,
                              PROJECT_VERSION_PATCH);
     try {
         auto sdl  = std::make_shared<Nutra::Core::SDL_Instance>();
         auto game = std::make_unique<Nutra::Game::Application>(sdl, PROJECT_NAME);
         game->run();
     } catch (std::exception & e) {
-        std::cerr << e.what() << std::endl;
+        spdlog::error("An error occured during starting: {}", e.what());
         return -1;
     }
     return 0;
